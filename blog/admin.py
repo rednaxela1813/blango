97f5674dd40a7cc78afb4c6ee8e0df01bbfbecc1
Alexander Kiselev <rednaxela1813@gmail.com>
2024-05-20 07:45:39 +0000

    First generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
-
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
