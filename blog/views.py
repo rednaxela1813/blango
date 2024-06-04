79f31fb46c14bd8da2ab3af21d3f0de508de792d
Alexander Kiselev <rednaxela1813@gmail.com>
2024-06-04 10:13:31 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,18 @@ from blog.models import Post
 from blog.forms import CommentForm
 import logging
 
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+
 
 logger = logging.getLogger(__name__)
 
+@cache_page(300)
+@vary_on_cookie
 def index(request):
+    
+    logger.debug("Index function is called!")
+    
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
